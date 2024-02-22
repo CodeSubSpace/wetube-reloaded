fbcab5e78d793999ecb4b1e80fc42407614e6283
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-02-22 16:09:35 +0900

    understanding middlewares

--- src/server.js
+++ src/server.js
@@ -4,8 +4,17 @@ const PORT = 4000;
 const app = express();
 
 // get request에 응답하는 방법 등을 application에게 먼저 만들어줘야 한다.
-const gossipMiddleware= (req, res, next) => {
-    console.log(`I'm in the middle! ${req.url}`);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url=='/protected') {
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    console.log("Allowed, you may continue.")
     next();
 }
 
@@ -13,7 +22,13 @@ const handleHome = (req, res) => {
     return res.send("I love mingyun");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+const handleProtected = (req, res) => {
+    return res.send("Welcome to private world!")
+}
+
+app.use(privateMiddleware);
+app.use(logger, handleHome);
+app.get("/private", handleProtected);
 
 // 그리고 나서 외부에 어플리케이션을 개방한다.
 const handleListening = () =>
