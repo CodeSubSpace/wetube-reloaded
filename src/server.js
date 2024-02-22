bb5b8af571c050d96823336112c53de28787782c
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-02-22 15:15:59 +0900

    create middleware

--- src/server.js
+++ src/server.js
@@ -4,19 +4,19 @@ const PORT = 4000;
 const app = express();
 
 // get request에 응답하는 방법 등을 application에게 먼저 만들어줘야 한다.
-const handleHome = (req, res) => {
-    // return res.end()
-    return res.end();
-};
-
-const handleLogin = (req, res) => {
-    return res.send("hey, login here!")
+const gossipMiddleware= (req, res, next) => {
+    console.log(`I'm in the middle! ${req.url}`);
+    next();
 }
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const handleHome = (req, res) => {
+    return res.send("I love mingyun");
+};
+
+app.get("/", gossipMiddleware, handleHome);
+
 // 그리고 나서 외부에 어플리케이션을 개방한다.
 const handleListening = () =>
     console.log(`Server listening on port 4000 🚀 http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening); //request 대기
+app.listen(PORT, handleListening); //request 대기  
