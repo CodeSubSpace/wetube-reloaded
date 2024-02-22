21fde8cd54df266a383cf060e355296c762a9514
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-02-22 14:41:02 +0900

    understanding intercation between server and browser

--- src/server.js
+++ src/server.js
@@ -4,12 +4,19 @@ const PORT = 4000;
 const app = express();
 
 // get request에 응답하는 방법 등을 application에게 먼저 만들어줘야 한다.
-const handleHome = () => console.log("Somebody is trying to go home");
-app.get("/", handleHome);
+const handleHome = (req, res) => {
+    // return res.end()
+    return res.end();
+};
 
+const handleLogin = (req, res) => {
+    return res.send("hey, login here!")
+}
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 // 그리고 나서 외부에 어플리케이션을 개방한다.
 const handleListening = () =>
     console.log(`Server listening on port 4000 🚀 http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening); //request 대기
-
