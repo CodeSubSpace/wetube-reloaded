0204559a93af147d1f8b385fda0499196f0f591c
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-02-25 01:12:14 +0900

    create pug file (intentional error occuring)

--- src/server.js
+++ src/server.js
@@ -3,12 +3,16 @@ import morgan from "morgan"
 import globalRouter from './routers/globalRouters.js'
 import userRouter from './routers/userRouter.js'
 import videoRouter from './routers/videoRouter.js'
+import path from 'path';
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 app.use(logger);
+
+app.set("view engine", "pug");
+// app.set("views", path.join(process.cwd(), "src/views"));
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
