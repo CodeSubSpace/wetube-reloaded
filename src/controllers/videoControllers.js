e63f040693c9686626ba6ea1d8f42ac07d9e8601
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-02-25 23:37:22 +0900

    Used Conditional in PUG

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,9 +1,14 @@
-export const trending = (req, res) => res.render("home", {pageTitle:"Home"});
-export const see = (req, res) => res.render("watch", {pageTitle:"Watch"});
-export const edit = (req, res) => res.render("edit", {pageTitle:"Edit"});
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res)  => res.send("upload");
-export const deleteVideo = (req, res)  => {
-    console.log(req.params);
-    return res.send("Delete video");
-}
+const fakeUser = {
+    username: "Nicolas",
+    loggedIn: false,
+  };
+  
+  export const trending = (req, res) =>
+    res.render("home", { pageTitle: "Home", fakeUser:fakeUser });
+  export const see = (req, res) => res.render("watch");
+  export const edit = (req, res) => res.render("edit");
+  export const search = (req, res) => res.send("Search");
+  export const upload = (req, res) => res.send("Upload");
+  export const deleteVideo = (req, res) => {
+    return res.send("Delete Video");
+  };
