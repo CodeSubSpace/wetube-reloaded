742c13c48d0918841737c803b9dbc223e7107278
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-03-05 17:16:05 +0900

    HTTP Method practice part two

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -54,6 +54,14 @@ export const getUpload = (req, res) => {
 }
 
 export const postUpload = (req, res) => {
-  // here we will add a video to the videos array
+  const newVideo = {
+    title : req.body.title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views : 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
   return res.redirect("/");
 }
