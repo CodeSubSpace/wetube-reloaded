99b0d1bfd2d4349cc67900dc38979e7a95e4652a
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-03-05 16:43:00 +0900

    Router's route method

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,7 +3,7 @@ import { watch, getEdit, postEdit } from "../controllers/videoControllers";
 
 
 const videoRouter = express.Router();
-videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)").get(watch)
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit)
 
 export default videoRouter;
