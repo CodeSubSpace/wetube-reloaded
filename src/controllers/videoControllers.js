beab64053821d038190e9285af0d79bd7f207e56
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-03-11 16:28:42 +0900

    Edit Video part Two

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -29,14 +29,22 @@ export const getEdit = async (req, res) => {
   return res.render("edit", {pageTitle: `Edit: ${video.title}`, video});
 }
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body
-  return res.redirect(`/videos/${id}`);
-};
+  const {title, description, hashtags} = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", {pageTitle: "Video not found!"});
+  }
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags.split(",").map(word=>word.startsWith('#') ? word : `#${word}`);
+  await video.save();
+ return res.redirect(`/videos/${id}`);
+}
 
 export const getUpload = (req, res) => {
-  return res.render("upload", { pageTitle: `Upload Video`});
+  return res.redirect(`/videos/${id}`);
 }
  
 export const postUpload = async (req, res) => {
@@ -46,7 +54,7 @@ export const postUpload = async (req, res) => {
       title,
       description,
       createdAt: Date.now(),
-      hashtags: hashtags.split(",").map(word=>`#${word}`),
+      hashtags: hashtags.split(",").map(word=>word.startsWith('#') ? word : `#${word}`),
       meta: {
         views: 0,
         rating: 0,
