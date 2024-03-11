f1012fd3488c8d92784f458aa2e479315b653757
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-03-11 12:00:14 +0900

    Creating a Video Part Two

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -31,11 +31,11 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: `Upload Video`});
 }
  
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = new Video({
-    title: title,
-    description: description,
+    title,
+    description,
     createdAt: Date.now(),
     hashtags: hashtags.split(",").map(word=>`#${word}`),
     meta: {
@@ -43,6 +43,7 @@ export const postUpload = (req, res) => {
       rating: 0,
     }
   })
-  console.log(video);
+	const dbVideo = await video.save();
+  console.log(dbVideo);
   return res.redirect("/");
 }
