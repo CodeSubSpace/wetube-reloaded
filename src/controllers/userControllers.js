f03bcfa3b3153f567b4c5e01a51eee1dd6a81c9f
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-03-18 15:53:03 +0900

    Creating Account part Two

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,12 +1,22 @@
+import User from "../models/User"
+
 export const getJoin = (req, res) => res.render(
     "join", 
     {pageTitle: "Join"}
 );
 
-export const postJoin = (req, res) => res.render(
-    console.log(req.body),
-    res.end()
-);
+export const postJoin = async(req, res) => {
+    const {name, email, username, password, location} = req.body;
+    console.log(name, username, email, password, location)
+    await User.create({ //이 중괄호를 쓰지 않으면 인자와 관련된 오류가 발생함.
+        name, 
+        email, 
+        username, 
+        password, 
+        location,
+    })
+    res.redirect("/login");
+};
 
 export const edit = (req, res) => res.send("Edit user")
 export const remove = (req, res) => res.send("Delete user")
