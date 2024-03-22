41e9bb622955b9004c46aad21e79562cd8188351
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-03-22 11:24:09 +0900

    From Validation

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -6,13 +6,27 @@ export const getJoin = (req, res) => res.render(
 );
 
 export const postJoin = async(req, res) => {
-    const {name, email, username, password, location} = req.body;
-    console.log(name, username, email, password, location)
+    const {name, email, username, password1, password2, location} = req.body;
+    const exists = await User.exists({ $or: [{ username }, { email }]});
+    if (exists) {
+        return res.render("join", {
+            pageTitle: "join",
+            errorMessage: "This username/email is already taken."
+        });
+    }
+    
+    if(password1 !== password2) {
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "Password confirmation does not match",
+        })
+    }
+
     await User.create({ //이 중괄호를 쓰지 않으면 인자와 관련된 오류가 발생함.
         name, 
         email, 
         username, 
-        password, 
+        password1, 
         location,
     })
     res.redirect("/login");
