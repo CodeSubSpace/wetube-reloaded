e2e3e7bdaf1077fcddef995669e4c6958527e51a
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-03-28 11:50:51 +0900

    Sessions and Cookies part One

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan"
+import session from "express-session";
 import rootRouter from './routers/rootRouters.js'
 import userRouter from './routers/userRouter.js'
 import videoRouter from './routers/videoRouter.js'
@@ -16,6 +17,13 @@ app.set("views", path.join(process.cwd(), "src/views"));
 
 app.use(express.urlencoded({ etended: true}));
 
+app.use(
+    session({
+        secret:"Hello!",
+        resave: true,
+        saveUninitialized: true
+    })
+);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
