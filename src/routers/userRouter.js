8f37ce70ba1db5eab4b3b59389669680abbef88b
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-04-15 16:51:04 +0900

    Edit Profile GET

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { edit, remove, see, logout, startGithubLogin, finishGithubLogin} from "../controllers/userControllers"
+import { edit, 
+         remove,
+         getEdit,
+         postEdit, 
+         see, 
+         logout, 
+         startGithubLogin, 
+         finishGithubLogin} from "../controllers/userControllers"
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.get("/remove", remove);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
