cd7118f3d9d9e2f3d5318ecdde2629818947c72c
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-05-02 11:14:41 +0900

    Time Formatting

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,12 +1,11 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
-
+const currenTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 let volumeValue = 0.5;
 video.volume = volumeValue;
-
 const handlePlayClick = (e) => {
   if (video.paused) {
     video.play();
@@ -15,7 +14,6 @@ const handlePlayClick = (e) => {
   }
   playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-
 const handleMuteClick = (e) => {
   if (video.muted) {
     video.muted = false;
@@ -25,7 +23,6 @@ const handleMuteClick = (e) => {
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
   volumeRange.value = video.muted ? 0 : volumeValue;
 };
-
 const handleVolumeChange = (event) => {
   const {
     target: { value },
@@ -38,6 +35,21 @@ const handleVolumeChange = (event) => {
   video.volume = value;
 };
 
+const formatTime = (seconds) => {
+  return new Date(seconds * 1000).toISOString().substr(11, 8);
+}
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+};
+
+const handleTimeUpdate = () => {
+  console.log(video.currentTime)
+  currenTime.innerText = formatTime(Math.floor(video.currentTime));
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
