03635b54b5fc6524d1fa91170d53ca3995f1c64b
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-05-02 15:33:57 +0900

    Controls Events part Two

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -8,7 +8,9 @@ const timeline = document.getElementById("timeline");
 const fullScreenBtn = document.getElementById("fullScreen");
 const videoContainer = document.getElementById("videoContainer");
 const videoControls = document.getElementById("videoControls");
+
 let controlsTimeout = null;
+let controlsMovementTimeout = null;
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -74,18 +76,23 @@ const handleFullScreen = () => {
   }
 }
 
+const hideControls = () => videoControls.classList.remove("showing");
+
 const handleMouseMove = () => {
   if (controlsTimeout) {
     clearTimeout(controlsTimeout);
     controlsTimeout = null;
   }
+  if (controlsMovementTimeout) {
+    clearTimeout(controlsMovementTimeout);
+    controlsMovementTimeout = null;
+  }
   videoControls.classList.add("showing");
+  controlsMovementTimeout = setTimeout(hideControls, 3000);
 };
 
 const handleMouseLeave = () => {
-  controlsTimeout = setTimeout(() => {
-    videoControls.classList.remove("showing");
-  }, 3000);
+  controlsTimeout = setTimeout(hideControls, 3000);
 };
 
 playBtn.addEventListener("click", handlePlayClick);
