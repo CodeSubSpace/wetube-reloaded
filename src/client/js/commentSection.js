405baa345a9e32ed52973aa0d84a39519781fbd6
CodeSubSpace <bellwest@yonsei.ac.kr>
2024-05-14 11:13:49 +0900

    Rendering Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,27 +1,23 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-
-const handleSubmit = (event) => {
-    event.preventDefault();
-    const textarea = form.querySelector("textarea");
-    const btn = form.querySelector("button");
-    let text = textarea.value;
-    const videoId = videoContainer.dataset.id;
-    
-    if (text === "") {
-        return
-    };
-    fetch(`/api/videos/${videoId}/comment`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text })
-    })
-    textarea.value = "";
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const textarea = form.querySelector("textarea");
+  const text = textarea.value;
+  const videoId = videoContainer.dataset.id;
+  if (text === "") {
+    return;
+  }
+  await fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
+  textarea.value = "";
 };
-
 if (form) {
-form.addEventListener("submit", handleSubmit);
+  form.addEventListener("submit", handleSubmit);
 }
